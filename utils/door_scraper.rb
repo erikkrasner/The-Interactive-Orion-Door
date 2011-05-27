55d34e946b7627aeb4aac668760a5696005bdfeb
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-05-27 16:05:42 -0400

    scraper script gathers the correct information

---
+++ utils/door_scraper.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require "nokogiri"
+
+door_num = 1
+while true
+  uri = URI.parse("http://www-space.arc.nasa.gov/~rubin/door#{door_num}.html")
+  http = Net::HTTP.new(uri.host,uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  if response.code == "404"
+    break
+  end
+  body = response.body
+  doc = Nokogiri::HTML(body)
+  tds = doc.xpath("//td").each { |this|
+    begin
+      a = this > 'a'
+      imgsrc = a[0]['href']
+      timg = a > 'img'
+      timgsrc = timg[0]['src']
+      txt = a.inner_text.strip
+      contributor = this.inner_text.scan(/\([^)]+\)/)[-1]
+      if !contributor
+        contributor = "(Contributed by Bob Rubin)"
+      end
+    
+      puts(imgsrc)
+      puts(timgsrc)
+      puts(txt)
+      puts(contributor)
+    rescue
+    end
+  }
+  
+  door_num += 1
+end
