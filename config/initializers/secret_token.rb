61fc76377946d7fd43ef3c2f55d282b217d8d79a
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-05-26 21:47:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrionDoor::Application.config.secret_token = '19a6dd01c31406abf326dc9c6aefc2f121caec190a1ad3d2b54e3113990db101ced4ec13bbb34b0a416c9a3e63f67988bf7a6d5e83d9f34abeb48c411c49bbdc'
