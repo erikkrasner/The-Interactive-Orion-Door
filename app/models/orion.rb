2919b60e95d0819b2d08d03b64805ca45084566d
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-05-27 23:37:06 -0400

    more basic navigation logic worked out

--- app/models/orion.rb
+++ app/models/orion.rb
@@ -1,2 +1,8 @@
 class Orion < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :contributor, :presence => true
+  validates :imgsrc, :presence => true
+  validates :timgsrc, :presence => true
+  validates :x, :presence => true
+  validates :y, :presence => true
 end
